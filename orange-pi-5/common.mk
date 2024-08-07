bf5c5591de6d90c36af288cd72228f067f88088c
FooFooDamon <udc577@126.com>
2024-08-07 13:38:49 +0800

    orange-pi-5/common.mk: Add targets: bootscript_install, bootscript and fix_clangd_db

--- orange-pi-5/common.mk
+++ orange-pi-5/common.mk
@@ -4,4 +4,22 @@ INSTALL_DIR ?= ${HOME}/tftpd/orange-pi-5
 DEFCONFIG ?= configs/orangepi_5_defconfig
 EXT_TARGETS += dtbs
 CUSTOM_FILES += arch/${ARCH}/dts/rk3588s-orangepi-5.dts
+USER_HELP_PRINTS ?= echo "  * ${MAKE} bootscript"; \
+    echo "  * ${MAKE} bootscript_install"; \
+    echo "  * ${MAKE} fix_clangd_db";
+
+.PHONY: bootscript bootscript_install
+
+#install: bootscript_install
+
+bootscript_install: bootscript
+	install ../boot.cmd ../boot.scr /boot/
+
+bootscript: ../boot.scr
+
+../boot.scr: ../boot.cmd
+	mkimage -C none -A arm -T script -d $< $@
+
+fix_clangd_db:
+	sed -i -e '/"-f/d' -e '/"-mabi=lp64"/d' ${SRC_ROOT_DIR}/compile_commands.json
 
