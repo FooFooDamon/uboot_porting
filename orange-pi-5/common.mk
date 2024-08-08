aa9142501c43ed0701f9db28944834e4eabeed79
FooFooDamon <udc577@126.com>
2024-08-08 13:02:20 +0800

    orange-pi-5/common.mk: Add targets: u-boot.itb and bl31.elf

--- orange-pi-5/common.mk
+++ orange-pi-5/common.mk
@@ -2,13 +2,24 @@ ARCH ?= arm
 CROSS_COMPILE ?= aarch64-linux-gnu-
 INSTALL_DIR ?= ${HOME}/tftpd/orange-pi-5
 DEFCONFIG ?= configs/orangepi_5_defconfig
-EXT_TARGETS += dtbs
+EXT_TARGETS += u-boot.itb u-boot.dtb
 CUSTOM_FILES += arch/${ARCH}/dts/rk3588s-orangepi-5.dts
 USER_HELP_PRINTS ?= echo "  * ${MAKE} bootscript"; \
     echo "  * ${MAKE} bootscript_install"; \
     echo "  * ${MAKE} fix_clangd_db";
 
-.PHONY: bootscript bootscript_install
+.PHONY: u-boot.itb bl31.elf u-boot.dtb install bootscript bootscript_install fix_clangd_db
+
+u-boot.itb: bl31.elf u-boot.dtb
+
+bl31.elf:
+	bl31_dir=$$(${MAKE} showvars -s | grep SRC_ROOT_DIR | awk -F = '{ print $$2 }'); \
+	[ ! -s $${bl31_dir}/$@ ] || exit 0; \
+	sed -i '1s/\(python\)2/\1/' ${SRC_ROOT_DIR}/arch/arm/mach-rockchip/decode_bl31.py; \
+	wget -O $${bl31_dir}/$@ 'https://github.com/armbian/rkbin/raw/master/rk35/rk3588_bl31_v1.45.elf'; \
+	[ $$? -ne 0 ] || exit 0; \
+	rm -f $${bl31_dir}/$@; \
+	false
 
 #install: bootscript_install
 
